9cebc78a356bdc37e1b5cf5e4ef311a1e67c5705
analariosmed <ana.lariosma@gmail.com>
2024-10-06 12:21:30 -0400

    vercel

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const cors = require('cors')
+//allow origin from all
+app.use(cors())
 
 
 app.get("/api", (req, res) => { 
@@ -13,5 +16,57 @@ server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
 const json = `
-{"portfolio": "Hello World"}
+{"portfolio": [
+    {
+      "name": "Project 1",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 2",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 3",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 4",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 5",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 6",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 7",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 8",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 9",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    },
+    {
+      "name": "Project 10",
+      "description": "This is a project",
+      "url": "https://www.google.com"
+    }
+  ]
+}
 `

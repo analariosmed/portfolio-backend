ed90af26e06de48e6a1262a2573449df3f90e216
analariosmed <ana.lariosma@gmail.com>
2024-10-06 13:14:19 -0400

    no cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const cors = require('cors')
-app.use(cors())
+// const cors = require('cors')
+// app.use(cors())
 
 app.get("/", (req, res) => { 
   res.type('html').send("<h1>Portfolio api</h1>")
@@ -12,10 +12,7 @@ app.get("/api", (req, res) => {
   res.type('json').send(json)
 });
 
-const server = app.listen(port, () => console.log(`Portfolio app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.listen(port, () => console.log(`Portfolio app listening on port ${port}!`));
 
 const json = `
 {"portfolio": [
